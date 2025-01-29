8cd69ab32e96b90676319b5b140cab8fdc211cb9
Ajeet <ajeets.shah.87@gmail.com>
2025-01-30 01:51:07 +0530

    Add BS

---
+++ src/stackoverflow/MyButton.tsx
@@ -0,0 +1,11 @@
+import Button from "react-bootstrap/Button";
+
+export function MyButton() {
+  return (
+    <>
+      <Button as="a" variant="primary">
+        Button as link
+      </Button>
+    </>
+  );
+}
